50e2d1bb687fb4726a93a9237c83a105ac0199e4
Your Name <you@example.com>
2019-07-17 12:08:03 +0100

    app scaffolding in place

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from './ppl-logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={Logo} alt="logo" />
+        <h1>
+          Supported Charities
+        </h1>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
