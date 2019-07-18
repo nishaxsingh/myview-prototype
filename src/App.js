eedf58107e3d9766f26db213a12e09be00fbf286
Your Name <you@example.com>
2019-07-18 14:44:30 +0100

    data is displayed in n a table

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import React from 'react';
 import Logo from './ppl-logo.png';
 import Charities from './charities.json'
 import './App.css';
-
+// import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 
 function App() {
   const items = Charities.charities.map( (charity) => {
-      return <li key={charity.charity}>{charity.type}, {charity.charity}, {charity.society}, {charity.description}</li>
+    return (
+    <TableRow key={charity.type}>
+    <TableCell component="th" scope="row">
+      {charity.type}
+    </TableCell>
+    <TableCell align="right">{charity.charity}</TableCell>
+    <TableCell align="right">{charity.society}</TableCell>
+    <TableCell align="right">{charity.description}</TableCell>
+    </TableRow>
+    );
   });
 
-console.log(items)
+  
 
   return (
     <div className="App">
@@ -20,11 +35,28 @@ console.log(items)
           Supported Charities
         </h1>
       </header>
-      <ul>
-        {items}
-      </ul>
+   
+    <Paper>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>type</TableCell>
+            <TableCell align="right">charity</TableCell>
+            <TableCell align="right">society</TableCell>
+            <TableCell align="right">description</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {items}
+        </TableBody>
+      </Table>
+    </Paper>
     </div>
-  );
+     );
+
+
+
+
 }
 
 export default App;
